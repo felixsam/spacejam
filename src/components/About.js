2236f2c5ab3ff1563af555b23142f93c8bd73bf3
Felix <felixsam@users.noreply.github.com>
2020-07-25 23:40:00 -0700

    update the about page

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,9 @@ const About = () => {
     <div>
       <div className="container">
         <h4 className="center">About</h4>
-        <p>About Page Content</p>
+        <p>The purpose of this web app is to show the functionalities of the API's provided by NASA to explore Space</p>
+        <p>There is no affiliation or collaboration with NASA. </p>
+        <p> Logos and trademarks including the NASA Insignia are property of NASA </p>
       </div>
 
     </div>
